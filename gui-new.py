e11718ba3365315e7ec49e5a5ce68dfa3c5b1526
quentin-tracy <q314tracy@gmail.com>
2025-01-03 15:57:09 -0600

    Update gui-new.py
    added parametrics for window sizing
    working on automatically scanning \sounds\ dir and making appropriate buttons automatically

--- gui-new.py
+++ gui-new.py
@@ -2,47 +2,65 @@ import sys
 import playsound
 import os
 from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
+from PyQt5.QtGui import QFont
 
 #button spacing parameters
 button_number = 5
 button_spacing_x = 10
 button_spacing_y = 10
-button_width = 150
-button_height = 50
+button_width = 300
+button_height = 100
 
-#strings for buttons
-bunglers_list = ["Bunglers", r'\sounds\bunglers.mp3']
-laugh_list = ["Laugh", r'\sounds\laugh.mp3']
-cabbage_list = ["Cabbage", r'\sounds\cabbage.mp3']
-moron_list = ["Moron", r'\sounds\moron.mp3']
-scientist_list = ["Scientist", r'\sounds\scientist.mp3']
+#window sizing parameters
+window_size = [2, 4] #table of buttons
+window_width = (window_size[0] * button_width) + ((window_size[0] + 1) * button_spacing_x)
+window_height = (window_size[1] * button_height) + ((window_size[1] + 1) * button_spacing_y)
 
+#font options
+font = QFont()
+font.bold()
+font.setPointSize(20)
+
+#string lists for buttons
+#directory strings must be raw due to backslashes
+bunglers = ["BUNGLERS", r'\sounds\bunglers.mp3']
+laugh = ["LAUGH", r'\sounds\laugh.mp3']
+cabbage = ["CABBAGE", r'\sounds\cabbage.mp3']
+moron = ["MORON", r'\sounds\moron.mp3']
+scientist = ["SCIENTIST", r'\sounds\scientist.mp3']
+suitcase = ["SUITCASE", r'\sounds\suitcase.mp3']
+wimplash = ["WIMPLASH", r'\sounds\wimplash.mp3']
+
+#window class
 class window(QMainWindow):
     #init
     def __init__(self):
         super().__init__()
         #main window
-        self.setWindowTitle("Window")
+        self.setWindowTitle("Skeletor is my OTP")
         self.setGeometry(
+            500,
             500, 
-            500, 
-            1000,
-            500)
+            window_width,
+            window_height)
         #buttons
-        self.button0 = self.create_button(0, 0, bunglers_list[0], bunglers_list[1])
-        self.button1 = self.create_button(1, 0, cabbage_list[0], cabbage_list[1])
-        self.button2 = self.create_button(2, 0, laugh_list[0], laugh_list[1])
-        self.button3 = self.create_button(3, 0, moron_list[0], moron_list[1])
-        self.button4 = self.create_button(4, 0, scientist_list[0], scientist_list[1])
+        self.button0 = self.create_button(0, 0, bunglers[0], bunglers[1])
+        self.button1 = self.create_button(1, 0, cabbage[0], cabbage[1])
+        self.button2 = self.create_button(0, 1, laugh[0], laugh[1])
+        self.button3 = self.create_button(1, 1, moron[0], moron[1])
+        self.button4 = self.create_button(0, 2, scientist[0], scientist[1])
+        self.button5 = self.create_button(1, 2, suitcase[0], suitcase[1])
+        self.button6 = self.create_button(0, 3, wimplash[0], wimplash[1])
 
     #function for button creation
     def create_button(self, x:int, y:int, name:str, file_dir:str):
         button = QPushButton(f"{name}", self)
         button.setGeometry(
-            int(x * button_width + button_spacing_x),
-            int(y * button_height + button_spacing_y),
+            int((x * button_width + button_spacing_x * x) + button_spacing_x),
+            int((y * button_height + button_spacing_y * y) + button_spacing_y),
             int(button_width),
             int(button_height))
+        button.setFont(font)
         button.clicked.connect(lambda: self.play_sound(file_dir))
 
     #function for playing sounds
